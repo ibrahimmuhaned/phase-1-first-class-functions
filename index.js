a4e6326a9251bddd4f617ce3f8103e37467cb73d
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-27 23:04:43 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// Define the receivesAFunction function
 function receivesAFunction(callback) {
   callback();
 }
 
-// Define the returnsANamedFunction function
 function returnsANamedFunction() {
   return function namedFunction() {
     console.log("This is a named function");
   };
 }
 
-// Define the returnsAnAnonymousFunction function
 function returnsAnAnonymousFunction() {
   return function() {
     console.log("This is an anonymous function");
